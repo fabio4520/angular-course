3e896e92450c11dd8a15fed41ae8a5dee814804f
Fabio Fiestas <fabioleofc@gmail.com>
2023-08-14 05:15:35 -0500

    primera forma de usar el ngif

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,6 +14,12 @@ export class AppComponent implements AfterViewInit {
   imgUrl: string = "https://encrypted-tbn3.gstatic.com/licensed-image?q=tbn:ANd9GcSm50w7rt_NzsWgLVMYU2cE1fVryZw0tOWKc1kZxjOAntfDWPmZZAiAmTvL6J_TkWbwa1uZ43utnZkXsRI"
   boolClass: boolean = true;
   username: string;
+  postArray: Array<string> = ["post1", "post2", "post3"];
+  objArray: Array<any> = [
+    { id: 1, title: "post1", content: "content1" },
+    { id: 2, title: "post2", content: "content2" },
+    { id: 3, title: "post3", content: "content3" }
+  ]
 
   @ViewChild(PostComponent) postComponent: PostComponent;
 
@@ -40,5 +46,19 @@ export class AppComponent implements AfterViewInit {
 
   onKeyup2(username: string) {
     console.log(username);
+  }
+
+  addData($event: any) {
+    let max_id = 0;
+    for (let i = 0; i < this.objArray.length; i++) {
+      if (this.objArray[i].id > max_id) {
+        max_id = this.objArray[i].id;
+      }
+    }
+    this.objArray.push({ id: max_id + 1, title: $event.target.value, content: "" });
+
+  }
+  deleteData(index: number) {
+    this.objArray.splice(index, 1);
   }
 }
