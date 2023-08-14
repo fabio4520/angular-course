400e0e32ccb6035c531aa33aecaa35e4368bdf70
Fabio Fiestas <fabioleofc@gmail.com>
2023-08-14 09:34:44 -0500

    ADD postService and inject it into post.component

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,4 +1,6 @@
 import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
+import { PostService } from '../Services/post.service';
+
 
 @Component({
   selector: 'app-post',
@@ -11,11 +13,18 @@ export class PostComponent implements OnInit {
   messageFromPost: string = "message from post";
   childMessage: string = "message from child - Post";
   outputChildMessage: string = "message from child via output - Post";
+  postList: any[];
 
   @Input() fromParent: string;
   @Output() messageEvent = new EventEmitter<string>();
 
-  constructor() { }
+  constructor(
+    private PostService: PostService
+  ) {
+    // let PostServiceInstance = new PostService();
+    // this.postList = PostServiceInstance.postList;
+    this.postList = PostService.postList;
+  }
 
   ngOnInit() {
   }
